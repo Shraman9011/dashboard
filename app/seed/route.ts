3a8003f9b09bc9a6f9e29a67de9893bf01071ab2
Shraman9011 <shramansm108@gmail.com>
2025-04-22 16:51:39 +0530

    Your commit message

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,4 +1,6 @@
-import bcrypt from 'bcrypt';
+
+import bcrypt from 'bcryptjs'; // Use bcryptjs instead of bcrypt
+
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
 
